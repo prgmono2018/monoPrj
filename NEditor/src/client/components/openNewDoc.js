bf301abc154bb47f5415244fa21650ca0c733191
prgmono2018 <prgmono2018@gmail.com>
2019-09-10 12:40:37 +0300

    pagination fix

--- NEditor/src/client/components/openNewDoc.js
+++ NEditor/src/client/components/openNewDoc.js
@@ -7,15 +7,30 @@ export default class OpenNewDoc extends Component {
             //this.data=this.props.data;
         this.state={
             prjsPerPage:10,
-            currentPage:1
+            currentPage:1,
+            upperPageBound: 3,
+            lowerPageBound: 0,
+            isPrevBtnActive: 'disabled',
+            isNextBtnActive: '',
+            pageBound: 3
         }
         this.onOpenClick=this.onOpenClick.bind(this)
         this.onClickProject=this.onClickProject.bind(this)
         this.handleClick=this.handleClick.bind(this)
+
+
+        this.btnDecrementClick = this.btnDecrementClick.bind(this);
+        this.btnIncrementClick = this.btnIncrementClick.bind(this);
+        this.btnNextClick = this.btnNextClick.bind(this);
+        this.btnPrevClick = this.btnPrevClick.bind(this);
+        // this.componentDidMount = this.componentDidMount.bind(this);
+        this.setPrevAndNextBtnClass = this.setPrevAndNextBtnClass.bind(this);
+        this.preventEvents = this.preventEvents.bind(this);
     }
     componentDidMount () 
     {
-        
+        $("span.active").removeClass('active');
+        $('span#'+this.state.currentPage).addClass('active');
     }
     
      onClickProject(e,prjName){
@@ -48,33 +63,96 @@ export default class OpenNewDoc extends Component {
         var popup = document.getElementById("myPopup");
         //popup.innerHTML="Loading......"
         popup.classList.toggle("show");
-      /*  axios.get("http://localhost:7000/getAllProjects"
-        )
-        .then(
-         
-        json => {
-            popup.innerHTML="";
-            this.setState({data:json.data.docs});
-           // popup.innerHTML=this.printPrjList(json.data.docs);
-          }
-         
-        ).catch(error => {
-         
-            console.log('error code=', error.response.status);
-          })
-*/
        
     }
 
     handleClick(event) {
         event.stopPropagation();
+        const id=event.target.id;
         this.setState({
-          currentPage: Number(event.target.id)
+          currentPage: Number(id)
         });
+        $("span.active").removeClass('active');
+        $('span#'+id).addClass('active');
+        this.setPrevAndNextBtnClass(id);
       } 
 
+      setPrevAndNextBtnClass(listid) {
+        
+        let totalPage = Math.ceil(this.props.data.length / this.state.prjsPerPage);
+        this.setState({isNextBtnActive: 'disabled'});
+        this.setState({isPrevBtnActive: 'disabled'});
+        if(totalPage === listid && totalPage > 1){
+            this.setState({isPrevBtnActive: ''});
+        }
+        else if(listid === 1 && totalPage > 1){
+            this.setState({isNextBtnActive: ''});
+        }
+        else if(totalPage > 1){
+            this.setState({isNextBtnActive: ''});
+            this.setState({isPrevBtnActive: ''});
+        }
+        $("span.active").removeClass('active');
+        $('span#'+listid).addClass('active');
+    }
+
+    btnIncrementClick(event) {
+      
+        this.preventEvents(event);
+        this.setState({upperPageBound: this.state.upperPageBound + this.state.pageBound});
+        this.setState({lowerPageBound: this.state.lowerPageBound + this.state.pageBound});
+        let listid = this.state.upperPageBound + 1;
+        this.setState({ currentPage: listid});
+        this.setPrevAndNextBtnClass(listid);
+       // $("span.active").removeClass('active');
+        //$('span#'+listid).addClass('active');
+  }
+    btnDecrementClick(event) {
+           
+      this.preventEvents(event);
+      this.setState({upperPageBound: this.state.upperPageBound - this.state.pageBound});
+      this.setState({lowerPageBound: this.state.lowerPageBound - this.state.pageBound});
+      let listid = this.state.upperPageBound - this.state.pageBound;
+      this.setState({ currentPage: listid});
+      this.setPrevAndNextBtnClass(listid);
+     // $("span.active").removeClass('active');
+      //$('span#'+listid).addClass('active');
+  }
+
+  btnPrevClick(event) {
+    this.preventEvents(event);
+    if((this.state.currentPage -1)%this.state.pageBound === 0 ){
+        this.setState({upperPageBound: this.state.upperPageBound - this.state.pageBound});
+        this.setState({lowerPageBound: this.state.lowerPageBound - this.state.pageBound});
+    }
+    let listid = this.state.currentPage - 1;
+    this.setState({ currentPage : listid});
+    this.setPrevAndNextBtnClass(listid);
+    //$("span.active").removeClass('active');
+    //$('span#'+listid).addClass('active');
+}
+btnNextClick(event) {
+   
+this.preventEvents(event);
+    if((this.state.currentPage +1) > this.state.upperPageBound ){
+        this.setState({upperPageBound: this.state.upperPageBound + this.state.pageBound});
+        this.setState({lowerPageBound: this.state.lowerPageBound + this.state.pageBound});
+    }
+    let listid = this.state.currentPage + 1;
+    this.setState({ currentPage : listid});
+    this.setPrevAndNextBtnClass(listid);
+    //$("span.active").removeClass('active');
+   // $('span#'+listid).addClass('active');
+}
+
+preventEvents(event){
+    event.stopPropagation(event)
+    event.preventDefault(event);
+}
     render () {
-        const { currentPage, prjsPerPage } = this.state;
+        const { currentPage, prjsPerPage,upperPageBound,lowerPageBound,isPrevBtnActive,isNextBtnActive} = this.state;
+        
+        
         //var prjs=this.props.data;
         var names = [];
         if (this.props.data.length>0){
@@ -85,6 +163,7 @@ export default class OpenNewDoc extends Component {
 
 /************ */
 
+           
         // Logic for displaying current Prjs
         const indexOfLastPrj = currentPage * prjsPerPage;
         const indexOfFirstPrj = indexOfLastPrj - prjsPerPage;
@@ -102,7 +181,7 @@ export default class OpenNewDoc extends Component {
           pageNumbers.push(i);
         }
         const rowLen = pageNumbers.length;
-        const renderPageNumbers = pageNumbers.map((number,index) => {
+     /*   const renderPageNumbers = pageNumbers.map((number,index) => {
             return (
                <b key={number}
                 id={number}
@@ -110,26 +189,59 @@ export default class OpenNewDoc extends Component {
                 </b>
              
             );
-          });
-
+          });*/
+          const renderPageNumbers = pageNumbers.map(number => {
+            if(number === 1 && currentPage === 1){
+                return(
+                    <span key={number} className='active' id={number}><a href='#' id={number} onClick={this.handleClick}>{number}</a></span>
+                )
+            }
+            else if((number < upperPageBound + 1) && number > lowerPageBound){
+                return(
+                   number==this.state.currentPage ? <span key={number} id={number} className='active'> <a href='#' id={number} onClick={this.handleClick}>{number}</a></span>: <span key={number} id={number}><a href='#' id={number} onClick={this.handleClick}>{number}</a></span>
+                )
+            }
+        });
+        let pageIncrementBtn = null;
+        if(pageNumbers.length > upperPageBound){
+            pageIncrementBtn = <span className=''><a href='#' onClick={this.btnIncrementClick}> &hellip; </a></span>
+        }
+        let pageDecrementBtn = null;
+        if(lowerPageBound >= 1){
+            pageDecrementBtn = <span className=''><a href='#' onClick={this.btnDecrementClick}> &hellip; </a></span>
+        }
+        let renderPrevBtn = null;
+        if(isPrevBtnActive === 'disabled') {
+            renderPrevBtn = <span className={isPrevBtnActive} onClick={this.preventEvents}><span id="btnPrev"> Prev </span></span>
+        }
+        else{
+            renderPrevBtn = <span className={isPrevBtnActive}><a href='#' id="btnPrev" onClick={this.btnPrevClick}> Prev </a></span>
+        }
+        let renderNextBtn = null;
+        if(isNextBtnActive === 'disabled') {
+            renderNextBtn = <span className={isNextBtnActive} onClick={this.preventEvents}><span id="btnNext"> Next </span></span>
+        }
+        else{
+            renderNextBtn = <span className={isNextBtnActive}><a href='#' id="btnNext" onClick={this.btnNextClick}> Next </a></span>
+        }
 
 /*********************** */
 
         return (
                   <div className="popup" onClick={this.onOpenClick} >Click me!
-                 <div style={{position: 'absolute'}}> 
-                            <div className="popuptext" id="myPopup">
-                            {currentPrjs}
-                            <div>{renderPageNumbers}</div>
-                            </div>
-                          
-                </div>
-
-</div>
-
-
-
-
+                        <div style={{position: 'absolute'}}> 
+                                    <div className="popuptext" id="myPopup">
+                                        {currentPrjs}
+                                        <div id="page-numbers" className="pagination pageinationWrapper">
+                                            {renderPrevBtn}
+                                            {pageDecrementBtn}
+                                            {renderPageNumbers}
+                                            {pageIncrementBtn}
+                                            {renderNextBtn}
+                                        </div>
+                                    </div>
+                        </div>
+                    </div>
 
         );
     }
